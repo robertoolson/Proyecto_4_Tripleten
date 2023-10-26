044aa9e150bb40a4bfbebd6417e93d5ba1732353
Roberto Olson <71295657+robertoolson@users.noreply.github.com>
2023-10-25 21:45:50 -0700

    Correct code for app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if disp_button: # al hacer clic en el botón
     st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
             
     # crear un grafico de dispersion
-    fig = px.scatter(car_data, x='year', y='price', color='manufacturer')
+    fig = px.scatter(car_data, y='odometer')
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
