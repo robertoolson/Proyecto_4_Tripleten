8321f247e79a48f1521bc364d923b6b7c7b51557
Roberto Olson <71295657+robertoolson@users.noreply.github.com>
2023-10-26 20:49:32 -0700

    Update app.py
    Moving the header to the top

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ import plotly.express as px
 import streamlit as st
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-
 st.header('Anuncios de venta de coches en EE. UU.') # escribir un título
+hist_button = st.button('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
